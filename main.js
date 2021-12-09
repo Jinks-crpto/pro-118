43059e26fca2296866fb77933ecc2590a2800a8c
Jinks-crpto <77697885+Jinks-crpto@users.noreply.github.com>
2021-12-09 17:52:48 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+song="";
+leftwristx="";
+leftwristy="";
+rightwristx="";
+rightwristy="";
+
+function setup()
+ { canvas = createCanvas(600, 500);
+     canvas.center(); 
+     video = createCapture(VIDEO); 
+     video.hide(); 
+
+     poseNet=ml5.poseNet(video,modelLoaded);
+     poseNet.on('pose', gotPoses)
+    }
+
+    function modelLoaded(){
+        console.log('PoseNet Is Initalized');
+    }
+    function gotPoses(results){
+        if(results.length>0)
+        {
+            console.log(results);
+            leftwristx=results[0].pose.leftWrist.x;
+            leftwristy=results[0].pose.leftWrist.y;
+            console.log("leftwristx = " + leftwristx +"leftwristy = " + leftwristy);
+            
+            rightwristx=results[0].pose.rightWrist.x;
+            rightwristy=results[0].pose.rightWrist.y;
+            console.log("rightwristx = " + rightwristx +"rightwristy = " + rightwristy);
+        
+        }
+    }
+
+     function draw(){
+image(video,0,0,600,500);
+}
+function preload(){
+    song=loadSound("music.mp3");
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
